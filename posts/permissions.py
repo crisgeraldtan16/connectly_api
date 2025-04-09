1150c17c8ba578426e2332231f98c6795e297246
lr.cgtan@mmdc.mcl.edu.ph <132436336+crisgeraldtan16@users.noreply.github.com>
2025-04-10 00:03:06 +0900

    Update permissions.py

--- posts/permissions.py
+++ posts/permissions.py
@@ -7,3 +7,12 @@ class IsPostAuthor(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user  # Ensure the request user is the post author
+
+class IsAdmin(BasePermission):
+    """
+    Custom permission to only allow admins to delete posts or comments.
+    """
+
+    def has_permission(self, request, view):
+        return request.user.is_staff  # Only admin users can perform certain actions
+
